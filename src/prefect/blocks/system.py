25df7225c989c25a1010989b3119a083194ea304
nate nowack <thrast36@gmail.com>
2024-08-17 11:25:03 -0500

    Update system.py

--- src/prefect/blocks/system.py
+++ src/prefect/blocks/system.py
@@ -119,7 +119,7 @@ class Secret(Block, Generic[T]):
         ```python
         from prefect.blocks.system import Secret
 
-        Secret(value="sk-1234567890").save("test-secret", overwrite=True)
+        Secret(value="sk-1234567890").save("BLOCK_NAME", overwrite=True)
 
         secret_block = Secret.load("test-secret")
 
